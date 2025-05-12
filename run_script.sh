1037b0c1d053a5352f366308c06dad97d74995d0
David Gordon <gordo893@umn.edu>
2025-05-12 18:54:47 -0500

    run_script.sh conda activate fastcn instead of snakemake so that scripts can run with richer python environment

--- run_script.sh
+++ run_script.sh
@@ -1,17 +1,17 @@
 #!/bin/bash -l
 
 # modified for maintenance window
-# #SBATCH --time=24:00:00
+#SBATCH --time=24:00:00
 
 # original:
-#SBATCH --time=96:00:00
+# #SBATCH --time=96:00:00
 
 #SBATCH --ntasks=1
 #SBATCH --mem=50g
 #SBATCH --tmp=1g
 
 source /projects/standard/hsiehph/shared/bin/initialize_conda.sh
-conda activate snakemake
+conda activate fastcn2
 
 
 set -euo pipefail
@@ -24,12 +24,16 @@ export PATH=$PWD/bin:$PATH
 
 snakemake \
     --use-conda \
-    -k \
     --configfile config/config.yaml \
-    --jobs 100 \
-    --profile profile \
+    --local-cores 20 \
+    --cores 20 \
+    --max-inventory-time 10000 \
+    --resources load=1000 \
+    --scheduler greedy \
     --latency-wait 60 \
     --restart-times 3 \
+    --jobs 100 \
+    --profile profile \
     "$@"
 
 
